a21ff04051de17a4e41e2075a13fd9f334aaf1f1
Zach <dnfjddk2@gmail.com>
2025-06-30 11:12:14 +0900

    Add problem '등산' with initial implementation and test cases

--- 등산/main.cpp
+++ 등산/main.cpp
@@ -5,6 +5,10 @@
 
 #include <iostream>
 #include <vector>
+
+#define DIRECTION 4
+#define INF 51*51+1
+
 using namespace std;
 
 
@@ -19,7 +23,7 @@ int main()
     const int SIZE = n * m;
 
     vector<int> map(SIZE);
-    vector<vector<int>> adj(SIZE, vector<int>(SIZE, 51 * 51 + 1));
+    vector adj(SIZE, vector(SIZE, INF));
 
     for (int i = 0; i < SIZE; ++i)
         adj[i][i] = 0;
@@ -28,52 +32,49 @@ int main()
     {
         for (int j = 0; j < m; ++j)
         {
-            cin >> map[i * n + j];
-            if (map[i * n + j] < 'a') map[i * n + j] -= 'A';
-            else map[i * n + j] -= 'a' - 26;
+            const int idx = i * m + j;
+            char input;
+            cin >> input;
+            if (input < 'a') map[idx] = input - 'A';
+            else map[idx] = input - 'a' + 26;
         }
     }
 
-    const int dx[4] = {-1, 1, 0, 0};
-    const int dy[4] = {0, 0, -1, 1};
     for (int i = 0; i < SIZE; ++i)
     {
-        const int x = i / n;
-        const int y = i % n;
-        for (int j = 0; j < 4; ++j)
+        const int x = i / m;
+        const int y = i % m;
+        for (int j = 0; j < DIRECTION; ++j)
         {
+            constexpr int dx[DIRECTION] = {-1, 1, 0, 0};
+            constexpr int dy[DIRECTION] = {0, 0, -1, 1};
             const int nx = x + dx[j];
             const int ny = y + dy[j];
-            const int ni = nx * n + ny;
+            const int ni = nx * m + ny;
+
             if (nx < 0 || nx > n - 1 || ny < 0 || ny > m - 1) continue;
-            if (map[ni] - map[i] <= t && map[ni] - map[i] >= -t)
+            if (map[ni] - map[i] > t || map[ni] - map[i] < -t) continue;
+
+            if (map[ni] > map[i])adj[i][ni] = (map[ni] - map[i]) * (map[ni] - map[i]);
+            else adj[i][ni] = 1;
+        }
+    }
+
+    for (int k = 0; k < SIZE; ++k)
+    {
+        for (int i = 0; i < SIZE; ++i)
+        {
+            for (int j = 0; j < SIZE; ++j)
             {
-                if (map[ni] > map[i])adj[i][ni] = (map[ni] - map[i]) * (map[ni] - map[i]);
-                else adj[i][ni] = 1;
+                if (adj[i][k] + adj[k][j] < adj[i][j]) adj[i][j] = adj[i][k] + adj[k][j];
             }
         }
     }
 
-    for (int i = 0; i < SIZE; ++i)
-    {
-        for (int j = 0; j < SIZE; ++j)
-        {
-            for (int k = 0; k < SIZE; ++k)
-            {
-                if (adj[j][i] + adj[i][k] < adj[j][k]) adj[j][k] = adj[j][i] + adj[i][k];
-            }
-        }
-    }
-
-    int result=0;
-    for (int i = 0; i < SIZE; ++i)
-    {
-        if (adj[0][i]+adj[i][0]<=d && map[i]>result)
-        {
-            result=map[i];
-        }
-    }
-    cout<<result;
+    int result = map[0];
+    for (int i = 1; i < SIZE; ++i)
+        if (adj[0][i] + adj[i][0] <= d && map[i] > result)result = map[i];
+    cout << result;
 
     return 0;
 }
